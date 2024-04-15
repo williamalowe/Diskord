153f98c751b0f1fdb1f0348a3c1f09cf367382d0
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-04-15 13:26:08 +1000

    fix: fix articleleft styles

--- src/components/ArticleLeft/ArticleLeft.jsx
+++ src/components/ArticleLeft/ArticleLeft.jsx
@@ -3,12 +3,14 @@ import styles from './ArticleLeft.module.css';
 const ArticleLeft = ({ header, text, img }) => {
   return (
     <article className={styles.article}>
-      <div className={styles.left}>
-        <h3>{header}</h3>
-        <p>{text}</p>
-      </div>
-      <div className={styles.right}>
-        <img src={img} alt="article 2 image" />
+      <div className={styles.content}>
+        <div className={styles.left}>
+          <h3>{header}</h3>
+          <p>{text}</p>
+        </div>
+        <div className={styles.right}>
+          <img src={img} alt="article 2 image" />
+        </div>
       </div>
     </article>
   )
