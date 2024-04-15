e8a9662260df69dc8afbb8b9cb41de13a47b17f1
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-04-15 13:31:19 +1000

    fix: remove herosection-component

--- src/components/HeroBanner/HeroBanner.jsx
+++ src/components/HeroBanner/HeroBanner.jsx
@@ -1,10 +1,12 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRightToBracket } from '@fortawesome/free-solid-svg-icons';
 import styles from './HeroBanner.module.css';
+import Navbar from '../Navbar/Navbar';
 
 const HeroBanner = () => {
   return (
     <div className={styles.banner}>
+      <Navbar />
       <div className={styles.content}>
         <h1>Imagine a place...</h1>
         <p>...where you can belong to a school club, a gaming group or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often.</p>
--- src/views/App/App.jsx
+++ src/views/App/App.jsx
@@ -1,20 +1,26 @@
 import ArticleLeft from '../../components/ArticleLeft/ArticleLeft';
 import ArticleRight from '../../components/ArticleRight/ArticleRight';
-import HeroSection from '../../components/HeroSection/HeroSection';
+import HeroBanner from '../../components/HeroBanner/HeroBanner';
 import styles from './App.module.css';
 
 const App = () => {
   return (
     <main className={styles.app}>
-      <HeroSection />
+      <HeroBanner />
       <ArticleRight  
         header='Create an invite-only place where you belong'
         text='Discord servers are organised into topic-based channels where you can collaborate, share and just talk about your day without clogging up a group chat.'
-        img='./article1.svg'/>
+        img='./article1.svg'
+      />
       <ArticleLeft 
         header='Where hanging out is easy'
         text='Grab a seat in a voice channel when you’re free. Friends in your server can see you’re around and instantly pop in to talk without having to call.'
         img='./article2.svg'
+      />
+      <ArticleRight  
+        header='From few to a fandom'
+        text='Get any community running with moderation tools and custom member access. Give members special powers, set up private channels and more.'
+        img='./article3.svg'
       />
     </main>
   )
