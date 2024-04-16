258f148dfed32b204bb2e63cdadf432a08118af4
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-04-16 13:39:33 +1000

    styles: add articleright whileinview motion

--- src/components/ArticleRight/ArticleRight.jsx
+++ src/components/ArticleRight/ArticleRight.jsx
@@ -1,15 +1,32 @@
 import styles from './ArticleRight.module.css';
+import { motion } from 'framer-motion';
 
 const ArticleRight = ({ header, text, img}) => {
   return (
     <article className={styles.article}>
-      <div className={styles.left}>
-        <img src={img} alt="article 1 image" />
-      </div>
-      <div className={styles.right}>
-        <h3>{header}</h3>
-        <p>{text}</p>
-      </div>
+      <motion.div 
+        className={styles.content}
+        initial={{
+          opacity: 0,
+          y: 20
+        }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+          transition: {
+            duration: 1
+          }
+        }}
+        viewport={{ once: true }}
+      >
+        <div className={styles.left}>
+          <img src={img} alt="article 1 image" />
+        </div>
+        <div className={styles.right}>
+          <h3>{header}</h3>
+          <p>{text}</p>
+        </div>
+      </motion.div>
     </article>
   )
 }
--- src/views/App/App.jsx
+++ src/views/App/App.jsx
@@ -4,17 +4,32 @@ import BannerArticle from '../../components/BannerArticle/BannerArticle';
 import Footer from '../../components/Footer/Footer';
 import FooterLogo from '../../components/FooterLogo/FooterLogo';
 import HeroBanner from '../../components/HeroBanner/HeroBanner';
+import { motion } from 'framer-motion';
 import styles from './App.module.css';
 
 const App = () => {
   return (
     <main className={styles.app}>
       <HeroBanner />
-      <ArticleRight  
-        header='Create an invite-only place where you belong'
-        text='Discord servers are organised into topic-based channels where you can collaborate, share and just talk about your day without clogging up a group chat.'
-        img='./article1.svg'
-      />
+      {/* <motion.div
+        initial={{
+          opacity: 0,
+          y: 20
+        }}
+        animate={{
+          opacity: 1,
+          y: 0,
+          transition: {
+            duration: 1
+          }
+        }}
+      > */}
+        <ArticleRight  
+          header='Create an invite-only place where you belong'
+          text='Discord servers are organised into topic-based channels where you can collaborate, share and just talk about your day without clogging up a group chat.'
+          img='./article1.svg'
+        />
+      {/* </motion.div> */}
       <ArticleLeft 
         header='Where hanging out is easy'
         text='Grab a seat in a voice channel when you’re free. Friends in your server can see you’re around and instantly pop in to talk without having to call.'
