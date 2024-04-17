29fb450c1c0f61e7b92e293ed40615b727808bb4
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-04-17 13:38:52 +1000

    feat: add mob-view articleleft

--- src/views/App/App.jsx
+++ src/views/App/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
           text='Discord servers are organised into topic-based channels where you can collaborate, share and just talk about your day without clogging up a group chat.'
           img='./article1.svg'
         />
-      {/* <ArticleLeft 
+      <ArticleLeft 
         header='Where hanging out is easy'
         text='Grab a seat in a voice channel when you’re free. Friends in your server can see you’re around and instantly pop in to talk without having to call.'
         img='./article2.svg'
@@ -25,7 +25,7 @@ const App = () => {
         text='Get any community running with moderation tools and custom member access. Give members special powers, set up private channels and more.'
         img='./article3.svg'
       />
-      <BannerArticle />
+      {/* <BannerArticle />
       <Footer />
       <FooterLogo /> */}
     </main>
