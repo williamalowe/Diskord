0ef66191e3a5cb5b45e3c5af8f80a831fe3b0899
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-04-17 13:29:17 +1000

    feat: add mob-view articleright

--- src/views/App/App.jsx
+++ src/views/App/App.jsx
@@ -10,12 +10,12 @@ const App = () => {
   return (
     <main className={styles.app}>
       <HeroBanner />
-        {/* <ArticleRight  
+        <ArticleRight  
           header='Create an invite-only place where you belong'
           text='Discord servers are organised into topic-based channels where you can collaborate, share and just talk about your day without clogging up a group chat.'
           img='./article1.svg'
         />
-      <ArticleLeft 
+      {/* <ArticleLeft 
         header='Where hanging out is easy'
         text='Grab a seat in a voice channel when you’re free. Friends in your server can see you’re around and instantly pop in to talk without having to call.'
         img='./article2.svg'
